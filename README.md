51b879fa0be66ad834160b244d215b850fbc4d2c
git@pierrefhapp.dev <git@pierrefhapp.dev>
2022-01-22 00:59:49 +0100

    looks like good code to me. commit this.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Small example repo for my post about git hooks.
+
+- the blog post: [here]()
+- the branch "no-hooks-commit-and-push" contain the files that would get into
+  the codebase without git hooks.
+- the main branch contains the files that did get into the codebase with git
+  hooks.
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+*
+* Author: Pierre Dahmani
+* Created: 22.01.2022
+*
+* Description: Example code that we are able to commit and push without any
+* problems if we have no git hooks set up.
+*/
+
+// this would be caught by eslint.
+var thisShouldBeConst = "hello world"
+
+// this would be formatted by prettier.
+console.log  ( "to many spaces...: "                                                 ,       thisShouldBeConst)
