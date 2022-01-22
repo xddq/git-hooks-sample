8771b95112019f3cd1ce14435f83284dda2f2680
git@pierrefhapp.dev <git@pierrefhapp.dev>
2022-01-22 01:28:37 +0100

    remove comments.

--- index.js
+++ index.js
@@ -7,9 +7,7 @@
  * problems if we have no git hooks set up.
  */
 
-// this would be caught by eslint.
 const thisShouldBeConst = "hello world";
 
-// this would be formatted by prettier.
 console.log("to many spaces...: ", thisShouldBeConst);
 console.log(" lets add another console log!");
