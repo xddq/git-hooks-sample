5f103b975d39c4a5eb2d65d4b281cc092c752fe7
git@pierrefhapp.dev <git@pierrefhapp.dev>
2022-01-22 01:10:07 +0100

    adds another console.log

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 /**
-*
-* Author: Pierre Dahmani
-* Created: 22.01.2022
-*
-* Description: Example code that we are able to commit and push without any
-* problems if we have no git hooks set up.
-*/
+ *
+ * Author: Pierre Dahmani
+ * Created: 22.01.2022
+ *
+ * Description: Example code that we are able to commit and push without any
+ * problems if we have no git hooks set up.
+ */
 
 // this would be caught by eslint.
-var thisShouldBeConst = "hello world"
+var thisShouldBeConst = "hello world";
 
 // this would be caught by eslint.
-const unusedVariable = "test"
+const unusedVariable = "test";
 
 // this would be formatted by prettier.
-console.log  ( "to many spaces...: "                                                 ,       thisShouldBeConst)
+console.log("to many spaces...: ", thisShouldBeConst);
+console.log(" lets add another console log!");
